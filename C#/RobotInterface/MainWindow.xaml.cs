e9a40f5f3ff34254db929eaa987a164163a95c40
TABLE3 <table3@gmail.com>
2021-03-15 18:49:33 +0100

    fin partie 1.1

---
+++ C#/RobotInterface/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RobotInterface
+{
+    /// <summary>
+    /// Logique d'interaction pour MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        int a = 0;
+        void SendMessage()
+        {
+            string message = textBoxEmission.Text;
+            textBoxReception.Text = textBoxReception.Text + "Reçu : " + message;
+            textBoxEmission.Text = "";
+        }
+        private void buttonEnvoyer_Click(object sender, RoutedEventArgs e)
+        {
+            
+            if (a == 1)
+            {
+                buttonEnvoyer.Background = Brushes.RoyalBlue;
+                a = 0;
+            }
+            else
+            {
+                buttonEnvoyer.Background = Brushes.Beige;
+                a = 1;
+            }
+
+            string message = textBoxEmission.Text;
+            textBoxReception.Text = textBoxReception.Text + "Reçu : "  + message + "\n";
+            textBoxEmission.Text = "";
+
+        }
+
+
+        private void textBoxEmission_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                SendMessage();
+            }
+        }
+    }
+}
