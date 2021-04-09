a77f81948cf69ab7b8d16286b08d09d736f5a38d
TABLE3 <table3@gmail.com>
2021-04-09 14:28:40 +0200

    fin 2.1

---
+++ Embedded/carte_moteur_mionnet.X/UART.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   UART.h
+ * Author: TP-EO-5
+ *
+ * Created on 9 avril 2021, 14:24
+ */
+
+#ifndef UART_H
+#define	UART_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* UART_H */
+
